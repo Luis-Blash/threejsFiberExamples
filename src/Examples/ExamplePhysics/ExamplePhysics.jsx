a90e18abcb602db46dd9b74914e69388f76bda0a
luis-blash <blash3g@gmail.com>
2023-08-23 15:45:28 -0600

    feat: pshycs

--- src/Examples/ExamplePhysics/ExamplePhysics.jsx
+++ src/Examples/ExamplePhysics/ExamplePhysics.jsx
@@ -7,6 +7,15 @@ import ScenePhysics from "./ScenePhysics";
 const ExamplePhysics = () => {
 	return (
 		<>
+			<p>
+				Rapier es para fisicas{" "}
+				<a href="https://rapier.rs/" target="_blank" rel="noopener noreferrer">
+					Link
+				</a>{" "}
+				<a href="https://www.npmjs.com/package/@react-three/rapier" target="_blank" rel="noopener noreferrer">
+					NPM
+				</a>
+			</p>
 			<div style={{ height: "100%" }}>
 				<Canvas
 					camera={{
--- src/Examples/ExamplePhysics/ScenePhysics.jsx
+++ src/Examples/ExamplePhysics/ScenePhysics.jsx
@@ -1,5 +1,53 @@
+import { CuboidCollider, Physics, RigidBody } from "@react-three/rapier";
+
 const ScenePhysics = () => {
-	return <></>;
+	return (
+		<>
+			<Physics debug>
+				<RigidBody colliders="ball">
+					<mesh castShadow position={[-2, 2, 0]}>
+						<sphereGeometry />
+						<meshStandardMaterial color="orange" />
+					</mesh>
+				</RigidBody>
+
+				<RigidBody>
+					<mesh castShadow position={[2, 2, 4]}>
+						<boxGeometry args={[3, 2, 1]} />
+						<meshStandardMaterial color="mediumpurple" />
+					</mesh>
+					<mesh castShadow position={[2, 2, 5]}>
+						<boxGeometry args={[1, 1, 1]} />
+						<meshStandardMaterial color="mediumpurple" />
+					</mesh>
+				</RigidBody>
+
+				<RigidBody>
+					<mesh castShadow position={[2, 2, -1]}>
+						<boxGeometry args={[3, 2, 1]} />
+						<meshStandardMaterial color="mediumpurple" />
+					</mesh>
+					<mesh castShadow position={[2, 2, 2]}>
+						<boxGeometry args={[1, 1, 1]} />
+						<meshStandardMaterial color="mediumpurple" />
+					</mesh>
+				</RigidBody>
+
+				<RigidBody colliders={false} position={[-5, 1, -0.25]} rotation={[Math.PI * 0.1, 0, 0]}>
+					<CuboidCollider args={[1.5, 1.5, 0.5]} />
+					<CuboidCollider args={[0.25, 1, 0.25]} position={[0, 0, 1]} rotation={[-Math.PI * 0.35, 0, 0]} />
+					{/* mesh */}
+				</RigidBody>
+
+				<RigidBody type="fixed">
+					<mesh receiveShadow position-y={-1.25}>
+						<boxGeometry args={[20, 0.5, 10]} />
+						<meshStandardMaterial color="greenyellow" />
+					</mesh>
+				</RigidBody>
+			</Physics>
+		</>
+	);
 };
 
 export default ScenePhysics;
